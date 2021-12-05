2f05938d07424a930e6c892d9b6ae40ce87fe9f1
KhanhLyn <95578053+KhanhLyn@users.noreply.github.com>
2021-12-05 23:57:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Đặng Thị Thùy Trang đây =)))",
-    introTitle: 'Hey boy',
+    introTitle: 'Hey Nguyễn Tiến Dũng',
     introDesc: `Trái đất vốn lạ thường
     Mà sao anh cứ đi nhầm đường
     Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^Click vào đây điiiii^^',
-    title: 'Cậu có iu tớ khum 🥰',
-    desc: 'Trả lời thật lòng nha -.- ',
-    btnYes: 'Đúng rùi nè hihi <33',
+    title: 'Đặng Thị Thùy Trang có đôi lời muốn gửi đến Nguyễn Tiến Dũng',
+    desc: 'Mở lòng với tớ đi',
+    btnYes: 'Đồng ý ạ <33',
     btnNo: 'Không nha :3',
-    question: 'Thế giới hơn 7 tỉ người mà sao cậu lại iu tớ <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    question: 'Cảm ơn cậu vì đã xuất hiện trong cuộc đời tớ :3',
+    btnReply: 'Gửi tình yêu đến ĐTTT <3',
+    reply: 'Yêu thì yêu không yêu thì yêu <3333333',
     mess: 'Tớ biết mà 🥰. Tớ cũng yêu cậu 😘😘',
-    messDesc: 'Hôm nay kết thúc Trái tim tình nguyện 2021 rồi, chúng ta cũng cùng kết thúc chuỗi ngày độc thân nha. Làm người yêu tớ nhé 🥰',
+    messDesc: 'Hôm nay kết thúc Trái tim tình nguyện 2021 rồi, chúng ta cũng cùng kết thúc chuỗi ngày độc thân nha. Làm người yêu tớ nhé 🐙🥰',
     btnAccept: 'Nhắn tin lại cho tớ đi nào <3',
     messLink: 'https://www.facebook.com/keen.2909',
 }
